f2ce462e069e0c526d622033724a48cd9d0a54ab
Deekshith-28 <103569557+Deekshith-28@users.noreply.github.com>
2022-11-02 20:30:14 +0530

    check

---
+++ polindrome.js
@@ -0,0 +1,13 @@
+let string="aman"
+let check=rev(string)
+if(string==check){
+console.log("polindrome")
+}else{
+ console.log("N/A")
+}
+
+function rev(str){
+  let sum=""
+  for(let i=0str.length-1;0<=i;i--){
+	sum+=str[i]
+}
